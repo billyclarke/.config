70eeb130d99be23ae247ccc9bfc5edc26abb3b87
William Clarke <william@clrk.io>
2016-06-05 15:04:32 -0400

    Adds bash/config file, and adds sourcing that file from .bashrc to set_symlinks

--- set_symlinks.sh
+++ set_symlinks.sh
@@ -2,3 +2,6 @@
 
 # Symlink ~/.vimrc to .config/vim/vimrc
 ln ./vim/vimrc ~/.vimrc
+
+# Add sourcing of .config/bash/config to default ~/.bashrc
+echo -e '\n# Source .config/bash/config for user-specific settings\nif [ -f ~/.config/bash/config ]; then\n    source ~/.config/bash/config\nfi' >> ~/.bashrc
