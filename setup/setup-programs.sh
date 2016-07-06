b408b54eb021b3e86b8549401c0bf32855bb0104
William Clarke <william@clrk.io>
2016-07-06 15:22:38 -0400

    Adds Google Chrome installation, removes sublime-text, cleans

--- setup/setup-programs.sh
+++ setup/setup-programs.sh
@@ -1,1 +1,20 @@
-apt-get install git htop i3-wm redshift sublime-text vim vlc
+#! /bin/bash
+
+# Add Google Chrome stable repository to apt
+echo 'Adding Google Chrome stable repository to apt'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+
+echo 'Running apt-get update'
+sudo apt-get update
+
+echo 'Installing specified packages'
+sudo apt-get install \
+       git \
+       google-chrome-stable \
+       htop \
+       i3-wm \
+       redshift \
+       shutter \
+       vim \
+       vlc
