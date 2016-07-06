19dd90b7531a53c7226d022dd65865c2d6e92f28
William Clarke <william@clrk.io>
2016-07-06 12:58:15 -0400

    Adds setup-git-config.sh for setting up the git repo in .config

---
+++ setup/setup-git-config.sh
@@ -0,0 +1,13 @@
+sudo apt-get install git
+
+cd ~/.config
+
+git init
+
+git remote add origin https://github.com/william-clarke/.config.git
+
+git pull origin master
+
+ssh-keygen -t rsa -b 2048
+
+apt-get install 
